ec53b306e320e7ea9a4a7736518279f335201d05
lolgingfreecss <matthewlaranjeiro@hotmail.ca>
2026-02-05 14:21:08 -0500

    Latest Temi SDK Version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,6 +37,7 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation("com.robotemi:sdk:1.136.0")
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
